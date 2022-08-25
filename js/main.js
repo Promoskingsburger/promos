e6e5fa6ba3eb89b07b192e6cef3ccadd94c731e5
nearias1301 <nearias1300101@gmail.com>
2022-08-25 15:50:51 -0300

    mi primer commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const elemntosCarousel = document.querySelectorAll(".carousel");
+    M.Carousel.init(elemntosCarousel, {
+        duration: 150,
+        dist: -80,
+        shift: 5,
+        padding: 5,
+        numVisible: 5,
+        indicators: true,
+
+    });
+});
